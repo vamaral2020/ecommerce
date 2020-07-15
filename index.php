7f29c9b360a03e230a9888794524313325e31d22
Vagner Ribeiro de Amaral <vagner2013.vrda@gmail.com>
2020-07-14 22:27:54 -0300

    page admin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -14,8 +15,16 @@ $app->get('/', function() {
 	$page = new Page();
 
 	$page->setTpl("index");
-    
-	
+    	
+});
+
+//link da administração
+$app->get('/admin', function() {
+
+	$page = new PageAdmin();
+
+	$page->setTpl("index");
+    	
 });
 
 $app->run();
