37d22b8f8051a8ea607a43a1458b758d0560215f
Vagner Ribeiro de Amaral <vagner2013.vrda@gmail.com>
2020-07-16 23:18:24 -0300

    erro codigo crud

--- index.php
+++ index.php
@@ -62,6 +62,83 @@ $app->get('/admin/logout', function(){
 	exit;
 });
 
+//hoje 16/07
+$app->get("/admin/users", function (){
+
+	User::verifylogin();
+
+	$users = User::ListAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users", array(
+		"users"=>$users
+
+	));
+
+
+});
+
+	$app->get("/admin/users", function (){
+
+	User::verifylogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users");
+
+	});
+
+
+	$app->get("/admin/users/create", function (){
+
+		User::verifylogin();
+
+		$page = new PageAdmin();
+
+		$page->setTpl("users-create");
+
+	});
+
+	$app->get("/admin/users/:iduser/delete", function ($iduser){
+
+	User::verifylogin();
+
+	});
+
+	$app->get('/admin/users/:iduser', function($iduser){
+			 
+			   User::verifyLogin();
+			 
+			   $user = new User();
+			 
+			   $user->get((int)$iduser);
+			 
+			   $page = new PageAdmin();
+			 
+			   $page ->setTpl("users-update", array(
+			        "user"=>$user->getValues()
+			    ));
+			 
+			});
+
+
+	$app->post("/admin/users/create", function (){
+
+		User::verifylogin();
+
+	});
+
+
+	$app->post("/admin/users/:iduser", function ($iduser){
+
+		User::verifylogin();
+
+	});
+
+
+
+
 $app->run();
 
  ?>
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -67,6 +67,29 @@ Class User extends Model {
 		$_SESSION[User::SESSION] = NULL;
 	}
 
+	public static function listAll()
+	{
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.despersons");
+	}
+
+			public function get($iduser)
+		{
+		 
+		 $sql = new Sql();
+		 
+		 $results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser;", array(
+		 ":iduser"=>$iduser
+		 ));
+		 
+		 $data = $results[0];
+		 
+		 $this->setData($data);
+		 
+		 }
+
 
 
 }
